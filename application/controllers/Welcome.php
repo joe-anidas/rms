624e64ccb89756f6778051e4f447373937914c87
JOE ANIDAS E <joeanidas.26it@licet.ac.in>
2025-04-18 14:12:43 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,14 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->database();
-        $this->load->model('Theme_model');
-        // Removed: $this->load->library('session');
-    }
+   
 
     public function index()
     {
