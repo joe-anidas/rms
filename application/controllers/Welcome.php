e00be8615287f0123aff0b5a27c33073685d4bfa
JOE ANIDAS E <joeanidas.26it@licet.ac.in>
2025-04-18 14:01:45 +0530

    commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,62 +3,63 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper('url');
-		$this->load->database();
-		$this->load->model('Theme_model');
-		$this->load->library('session');
-	}
-	public function index()
-	{
-		
-		$data['theme'] = $this->Theme_model->get_theme_path();
-		$this->load->view('header',$data);
-		$this->load->view('dashboard');
-		$this->load->view('footer');
-	}
-	public function registered_plot() {
-		
-		$data['theme'] = $this->Theme_model->get_theme_path();
-		$this->load->view('header',$data);
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->database();
+        $this->load->model('Theme_model');
+        // Removed: $this->load->library('session');
+    }
+
+    public function index()
+    {
+        $data['theme'] = $this->Theme_model->get_theme_path();
+        $this->load->view('header', $data);
+        $this->load->view('dashboard');
+        $this->load->view('footer');
+    }
+
+    public function registered_plot() {
+        $data['theme'] = $this->Theme_model->get_theme_path();
+        $this->load->view('header', $data);
         $this->load->view('registered_plot');
-		$this->load->view('footer');
+        $this->load->view('footer');
     }
-	public function garden_profile() {
-		
-		$data['theme'] = $this->Theme_model->get_theme_path();
-		$this->load->view('header',$data);
+
+    public function garden_profile() {
+        $data['theme'] = $this->Theme_model->get_theme_path();
+        $this->load->view('header', $data);
         $this->load->view('garden_profile');
-		$this->load->view('footer');
+        $this->load->view('footer');
     }
-	public function customer_details() {
-		
-		$data['theme'] = $this->Theme_model->get_theme_path();
-		$this->load->view('header',$data);
+
+    public function customer_details() {
+        $data['theme'] = $this->Theme_model->get_theme_path();
+        $this->load->view('header', $data);
         $this->load->view('customer_details');
-		$this->load->view('footer');
+        $this->load->view('footer');
     }
-	public function staff_details() {
-		
-		$data['theme'] = $this->Theme_model->get_theme_path();
-		$this->load->view('header',$data);
+
+    public function staff_details() {
+        $data['theme'] = $this->Theme_model->get_theme_path();
+        $this->load->view('header', $data);
         $this->load->view('staff_details');
-		$this->load->view('footer');
+        $this->load->view('footer');
     }
-	public function set_theme(){
-		$theme = $this->input->post('theme');
-		$user_id = "2";
-		$this->db->where('user_id', $user_id);
-		$query = $this->db->get('user_theme');
-		if($query->num_rows() > 0){
-			$this->db->where('user_id', $user_id);
-			$this->db->where('theme_id !=', $theme);
-			$this->db->update('user_theme', array('theme_name' => $theme));
-		}else{
-			$this->db->insert('user_theme', array('user_id' => $user_id, 'theme_name' => $theme));
-		}
+
+    public function set_theme(){
+        $theme = $this->input->post('theme');
+        $user_id = "2";
+        $this->db->where('user_id', $user_id);
+        $query = $this->db->get('user_theme');
         
-	}
+        if($query->num_rows() > 0){
+            $this->db->where('user_id', $user_id);
+            $this->db->where('theme_id !=', $theme);
+            $this->db->update('user_theme', array('theme_name' => $theme));
+        }else{
+            $this->db->insert('user_theme', array('user_id' => $user_id, 'theme_name' => $theme));
+        }
+    }
 }
